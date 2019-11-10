a991f47f8ef797f3ce442e507f0ef082335a2a46
JusethAg <mauagueda@gmail.com>
2019-11-10 17:38:29 -0600

    Optimize imports for whole project

--- kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
@@ -1,7 +1,6 @@
 package net.corda.training.state
 
 import net.corda.core.contracts.*
-import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
 import net.corda.training.contract.IOUContract
 import java.util.*
